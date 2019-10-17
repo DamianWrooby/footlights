c22e8f0c68f3ea6de522646c03e4dcab1524bf5b
Damian Wróblewski <dwroblewski89@gmail.com>
2019-10-17 21:41:47 +0200

    Background image

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -917,7 +917,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/axios/lib/helpers/bind.js":[function(require,module,exports) {
+},{"./img/footlights-logo-bg2.png":[["footlights-logo-bg2.9035e230.png","img/footlights-logo-bg2.png"],"img/footlights-logo-bg2.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/axios/lib/helpers/bind.js":[function(require,module,exports) {
 'use strict';
 
 module.exports = function bind(fn, thisArg) {
@@ -3019,7 +3019,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49426" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54661" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
